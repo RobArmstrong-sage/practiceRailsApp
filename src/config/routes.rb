ea756df8d4fffd1126c606834c62eaea2f89aab2
Rob Armstrong <rob.armstrong@sage.com>
2020-12-28 12:55:11 +0000

    backend functionality for deleting, creating, and showing recipes

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      post 'recipes/create'
+      get '/show/:id', to: 'recipes#show'
+      delete '/destroy/:id', to: 'recipes#destroy'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  get '/*path' => 'homepage#index'
 end
